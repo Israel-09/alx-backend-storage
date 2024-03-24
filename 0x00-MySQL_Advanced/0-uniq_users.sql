2138b0751346694ada07d80700977a6e8f126b58
israel-09 <israelinene91@gmail.com>
2024-03-24 21:05:44 +0100

    task 0 solution

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- create table, user with basic requirements
+
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
