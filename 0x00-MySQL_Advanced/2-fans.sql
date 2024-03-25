bc9c5ab73366e0c5fe3aec532d249ba0ba457c30
israel-09 <israelinene91@gmail.com>
2024-03-25 01:06:27 +0100

    task 2 solution

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- a SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
 
-SELECT origin, fans as nb_fansFROM
-FROM metal_bands 
+SELECT origin, SUM(fans) AS fans
+FROM metal_bands GROUP BY origin
 ORDER BY fans DESC;
---
+++ 0x00-MySQL_Advanced/greet.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+greet users
+"""
+
+
+def greet(name: str) -> None:
+    """greet by name"""
+    print("Welcome to new World,", name)
+
+greet("Israel")
